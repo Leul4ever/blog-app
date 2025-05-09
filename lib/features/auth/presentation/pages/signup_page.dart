9bbd092d2e7c3020e4f80e674c0f281af38a1d60
Leul Abera <liulabera43@gmail.com>
2025-05-09 05:14:41 -0700

    form validation

--- lib/features/auth/presentation/pages/signup_page.dart
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -1,46 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc_app/core/theme/app_pallete.dart';
 import 'package:flutter_bloc_app/features/auth/presentation/widgets/auth_field.dart';
 import 'package:flutter_bloc_app/features/auth/presentation/widgets/auth_gradient_button.dart';
 
-class SignupPage extends StatelessWidget {
+class SignupPage extends StatefulWidget {
   const SignupPage({super.key});
 
   @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  final emailController = TextEditingController();
+  final passwordControler = TextEditingController();
+  final nameController = TextEditingController();
+  final bool isObscuredText;
+
+  final formKey = GlobalKey<FormState>();
+  @override
+  void dispose() {
+    super.dispose();
+    emailController.dispose();
+    passwordControler.dispose();
+    nameController.dispose();
+    this.isObscuredText = false;
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: EdgeInsets.all(15),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Sign Up.',
-              style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            AuthField(hintText: ' Name '),
-            SizedBox(
-              height: 15,
-            ),
-            AuthField(hintText: 'Email'),
-            SizedBox(
-              height: 15,
-            ),
-            AuthField(hintText: 'password'),
-            SizedBox(
-              height: 20,
-            ),
-            AuthGradientButton(),
-            const SizedBox(height: 20),
-            RichText(
-              text: TextSpan(
-                text: "Don't have an account?",
-                style: Theme.of(context).textTheme.titleMedium,
+        padding: const EdgeInsets.all(15),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Sign Up.',
+                style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
               ),
-            ),
-          ],
+              const SizedBox(height: 30),
+              AuthField(hintText: ' Name ', controller: nameController),
+              const SizedBox(height: 15),
+              AuthField(hintText: 'Email', controller: emailController),
+              const SizedBox(height: 15),
+              AuthField(hintText: 'password', controller: passwordControler),
+              const SizedBox(height: 20),
+              const AuthGradientButton(),
+              const SizedBox(height: 20),
+              RichText(
+                text: TextSpan(
+                  text: "Don't have an account? ",
+                  style: Theme.of(context).textTheme.titleMedium,
+                  children: [
+                    TextSpan(
+                      text: 'Sign In ',
+                      style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                            color: AppPallete.gradient2,
+                            fontWeight: FontWeight.bold,
+                          ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
