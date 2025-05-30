ee94c4cf77b57a591ac0f1ec78cab16213765af0
Leul Abera <liulabera43@gmail.com>
2025-05-30 02:21:44 -0700

    progress bar in sign up

--- lib/features/auth/presentation/pages/signup_page.dart
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -30,58 +30,84 @@ class _SignupPageState extends State<SignupPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(15),
-        child: Form(
-          key: formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Sign Up .',
-                style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+      body: BlocConsumer<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state is AuthFailure) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text(state.message),
+                backgroundColor: Colors.red,
               ),
-              const SizedBox(height: 30),
-              AuthField(hintText: ' Name ', controller: nameController),
-              const SizedBox(height: 15),
-              AuthField(hintText: 'Email', controller: emailController),
-              const SizedBox(height: 15),
-              AuthField(
-                hintText: 'password',
-                controller: passwordController,
-                isObscuredText: true,
+            );
+          } else if (state is AuthSuccess) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text('Signup successful!'),
+                backgroundColor: Colors.green,
               ),
-              const SizedBox(height: 20),
-              AuthGradientButton(
-                buttonText: "Sign Up",
-                onPressed: () {
-                  if (formKey.currentState!.validate()) {
-                    context.read<AuthBloc>().add(AuthSignUp(
-                        email: emailController.text.trim(),
-                        password: passwordController.text.trim(),
-                        name: nameController.text.trim()));
-                  }
-                },
-              ),
-              const SizedBox(height: 20),
-              RichText(
-                text: TextSpan(
-                  text: "Already  have an account? ",
-                  style: Theme.of(context).textTheme.titleMedium,
-                  children: [
-                    TextSpan(
-                      text: 'Sign In ',
-                      style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                            color: AppPallete.gradient2,
-                            fontWeight: FontWeight.bold,
-                          ),
+            );
+            // TODO: Navigate to home page or login page
+          }
+        },
+        builder: (context, state) {
+          return Padding(
+            padding: const EdgeInsets.all(15),
+            child: Form(
+              key: formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Sign Up .',
+                    style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 30),
+                  AuthField(hintText: ' Name ', controller: nameController),
+                  const SizedBox(height: 15),
+                  AuthField(hintText: 'Email', controller: emailController),
+                  const SizedBox(height: 15),
+                  AuthField(
+                    hintText: 'password',
+                    controller: passwordController,
+                    isObscuredText: true,
+                  ),
+                  const SizedBox(height: 20),
+                  AuthGradientButton(
+                    buttonText:
+                        state is AuthLoading ? "Signing Up..." : "Sign Up",
+                    onPressed: state is AuthLoading
+                        ? () {}
+                        : () {
+                            if (formKey.currentState!.validate()) {
+                              context.read<AuthBloc>().add(AuthSignUp(
+                                  email: emailController.text.trim(),
+                                  password: passwordController.text.trim(),
+                                  name: nameController.text.trim()));
+                            }
+                          },
+                  ),
+                  const SizedBox(height: 20),
+                  RichText(
+                    text: TextSpan(
+                      text: "Already  have an account? ",
+                      style: Theme.of(context).textTheme.titleMedium,
+                      children: [
+                        TextSpan(
+                          text: 'Sign In ',
+                          style:
+                              Theme.of(context).textTheme.titleMedium?.copyWith(
+                                    color: AppPallete.gradient2,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
-          ),
-        ),
+            ),
+          );
+        },
       ),
     );
   }
--- lib/init_dependencies.dart
+++ lib/init_dependencies.dart
@@ -2,6 +2,7 @@ import 'package:flutter_bloc_app/core/secrets/app_secrets.dart';
 import 'package:flutter_bloc_app/features/auth/data/datasource/auth_remote_data_source.dart';
 import 'package:flutter_bloc_app/features/auth/data/repository/auth_repository_impl.dart';
 import 'package:flutter_bloc_app/features/auth/domain/repository/auth_repository.dart';
+import 'package:flutter_bloc_app/features/auth/domain/usecases/user_sign_up.dart';
 import 'package:flutter_bloc_app/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -21,6 +22,10 @@ void _initAuth(SupabaseClient supabase) {
 
   ServiceLocator.registerFactory<AuthRepository>(
       () => AuthRepositoryImpl(ServiceLocator()));
+
+  ServiceLocator.registerFactory<UserSignUp>(
+      () => UserSignUp(ServiceLocator()));
+
   ServiceLocator.registerLazySingleton(() => AuthBloc(
         userSignUp: ServiceLocator(),
       ));
